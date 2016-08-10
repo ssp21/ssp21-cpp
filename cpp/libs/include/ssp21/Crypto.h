8db56513b89a9c3571f34a8997e6f73829027cc8
Adam Crain <jadamcrain@gmail.com>
2016-08-10 11:36:07 -0400

    Added ssp21 library and mock Crypto class

---
+++ cpp/libs/include/ssp21/Crypto.h
@@ -0,0 +1,14 @@
+
+#ifndef SSP21_CRYTPTO_H
+#define SSP21_CRYTPTO_H
+
+namespace ssp21
+{
+	class Crypto
+	{
+	public:
+		Crypto();
+	};
+}
+
+#endif
---
+++ cpp/libs/src/ssp21/Crypto.cpp
@@ -0,0 +1,10 @@
+
+#include "ssp21/Crypto.h"
+
+namespace ssp21
+{
+	Crypto::Crypto()
+	{}
+}
+
+
