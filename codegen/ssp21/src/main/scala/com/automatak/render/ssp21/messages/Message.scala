4097253daefc776ce3f2bcf33076b6b0209cc40e
Adam Crain <jadamcrain@gmail.com>
2016-08-15 16:15:12 -0400

    move Field types into Message file

--- codegen/ssp21/src/main/scala/com/automatak/render/ssp21/messages/Message.scala
+++ codegen/ssp21/src/main/scala/com/automatak/render/ssp21/messages/Message.scala
@@ -1,5 +1,22 @@
 package com.automatak.render.ssp21.messages
 
+import com.automatak.render.EnumModel
+
+sealed trait Field {
+  def name : String
+}
+
+sealed case class U16(name: String) extends Field
+sealed case class U32(name: String) extends Field
+
+sealed case class Enum(model: EnumModel) extends Field {
+  def name : String = model.underscoredName
+}
+
+sealed case class Seq8ofU8(name: String) extends Field
+sealed case class Seq16ofU8(name: String) extends Field
+sealed case class Seq8ofSeq16ofU8(name: String) extends Field
+
 trait Message {
   def name : String
   def fields : List[Field]
