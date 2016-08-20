178ebe4ea1d2b02a554b55124da3ad79fd42c63f
jadamcrain <jadamcrain@gmail.com>
2016-08-20 00:30:43 -0400

    moved the sodium settings to their own cmake file so that they don't pollute the main build file

---
+++ cmake/inc/sodium.cmake
@@ -0,0 +1,39 @@
+
+
+
+if(WIN32)
+	
+	set(SODIUM_LIB_NAME libsodium)
+	# these definitions are required when linking libsodium statically on Windows
+	add_definitions(-DSODIUM_STATIC=1 -DSODIUM_EXPORT=)
+
+	if(NOT DEFINED SSP21_SODIUM_DIR)
+		MESSAGE( FATAL_ERROR "You must define SSP21_SODIUM_DIR")
+	endif()
+
+	if(NOT EXISTS "${SSP21_SODIUM_DIR}/include/sodium.h")
+	    MESSAGE( FATAL_ERROR "Can't find <sodium.h>. Check the SSP21_SODIUM_DIR")
+	endif()
+
+	include_directories(${SSP21_SODIUM_DIR}/include) 
+  
+	if(CMAKE_BUILD_TYPE STREQUAL "Release")
+		set(SODIUM_MSVC_BUILD_TYPE "Release")
+	else()
+		set(SODIUM_MSVC_BUILD_TYPE "Debug")
+	endif()
+
+	if(${MSVC_VERSION} EQUAL 1800)
+		set(SODIUM_MSVC_PATH v120)
+	else()
+		MESSAGE( FATAL_ERROR "Unknown MSVC version: ${MSVC_VERSION}")
+	endif()
+
+	set(SSP21_SODIUM_LIB_DIR "${SSP21_SODIUM_DIR}/Win32/Debug/${SODIUM_MSVC_PATH}/static")
+
+	MESSAGE("Using sodium libraries in ${SSP21_SODIUM_LIB_DIR}")
+	link_directories(${SSP21_SODIUM_LIB_DIR})
+
+else()
+	set(SODIUM_LIB_NAME sodium)
+endif()
