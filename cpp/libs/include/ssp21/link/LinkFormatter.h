283fc84c14dd565d79162918ea464a302932d9f5
Adam Crain <jadamcrain@gmail.com>
2016-08-23 14:32:26 -0400

    added test cases for the LinkFormatter

--- cpp/libs/include/ssp21/link/LinkFormatter.h
+++ cpp/libs/include/ssp21/link/LinkFormatter.h
@@ -3,13 +3,21 @@
 #define SSP21_LINKFORMATTER_H
 
 #include "openpal/util/Uncopyable.h"
-#include "openpal/container/Buffer.h"
 
-#include "Addresses.h"
+#include "openpal/container/RSlice.h"
+#include "openpal/container/WSlice.h"
+
+#include "ssp21/link/Addresses.h"
 
 namespace ssp21
 {	
+	class LinkFormatter : private openpal::StaticOnly
+	{
+	public:
 
+		// returns an empty slice if there wasn't sufficient space to write the frame
+		static openpal::RSlice write(openpal::WSlice dest, const Addresses& addr, const openpal::RSlice& payload);
+	};
 }
 
 #endif
