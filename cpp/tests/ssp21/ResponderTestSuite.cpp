ab6e4fe542bef37fcad4b07b30123a7f15849f02
jadamcrain <jadamcrain@gmail.com>
2016-08-25 13:26:41 -0400

    responder test suite now uses a fixture thats needed for every test case

--- cpp/tests/ssp21/ResponderTestSuite.cpp
+++ cpp/tests/ssp21/ResponderTestSuite.cpp
@@ -17,20 +17,31 @@
 using namespace ssp21;
 using namespace openpal;
 
-TEST_CASE(SUITE("can be constructed"))
+struct ResponderFixture
 {
-    MockLogHandler log("responder");
+	ResponderFixture(const Responder::Config& config = Responder::Config()) : 
+		log("responder"),
+		exe(),
+		lower(),
+		responder(config, exe, log.root.logger, lower)
+	{}
 
+	MockLogHandler log;
 	MockExecutor exe;
-    MockLowerLayer lower;
+	MockLowerLayer lower;
+	Responder responder;
+};
 
-    Responder responder(Responder::Config(), exe, log.root.logger, lower);
-    responder.on_open();
+TEST_CASE(SUITE("responds to malformed REQUEST_HANDSHAKE_BEGIN with bad_message_format"))
+{
+	ResponderFixture fix;
 
-    // request handshake begin with header only
-    lower.enqueue_message(Addresses(5, 5), "00");
-    responder.on_rx_ready();
+	fix.responder.on_open();
+
+    // request handshake begin (function only)
+	fix.lower.enqueue_message(Addresses(5, 5), "00");
+	fix.responder.on_rx_ready();
 
     // ReplyHandshakeError w/ error = bad message format
-    REQUIRE(lower.pop_tx_message() == "04 00");
+	REQUIRE(fix.lower.pop_tx_message() == "04 00");
 }
