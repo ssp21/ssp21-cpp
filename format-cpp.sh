c7ab64c7d55e808aff8138f2b57a0d3595c08184
jadamcrain <jadamcrain@gmail.com>
2016-08-25 09:33:25 -0700

    exclude generated code

--- format-cpp.sh
+++ format-cpp.sh
@@ -1,3 +1,7 @@
-astyle -R ./cpp/*.h ./cpp/*.cpp --options=./astyle.cfg
+astyle -R ./cpp/*.h ./cpp/*.cpp --options=./astyle.cfg \
+--exclude=./cpp/libs/include/ssp21/gen \
+--exclude=./cpp/libs/src/ssp21/gen \
+--exclude=./cpp/libs/include/ssp21/msg \
+--exclude=./cpp/libs/src/ssp21/msg
 
 
