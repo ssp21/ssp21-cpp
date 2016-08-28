6282ab4443fd2cffe8456c3ca384c146ef144624
jadamcrain <jadamcrain@gmail.com>
2016-08-27 21:26:40 -0400

    link formatter now uses safe write functions

--- cpp/libs/src/ssp21/link/LinkFormatter.cpp
+++ cpp/libs/src/ssp21/link/LinkFormatter.cpp
@@ -15,6 +15,8 @@ namespace ssp21
 {
 	RSlice LinkFormatter::write(WSlice dest, const Message& message)
     {
+		auto start = dest.as_rslice();
+
         if (dest.length() < consts::min_link_frame_size)
         {
             return RSlice::empty_slice();
@@ -35,28 +37,26 @@ namespace ssp21
         // safe cast since we've already validated the payload length relative to the maximum allowed
 		const auto payload_length = static_cast<uint16_t>(message.payload.length());
 
-        // there's enough space to write the entire frame
-        // const auto frame_size = consts::min_link_frame_size + payload.length();
+		BigEndian::write(
+			dest,
+			consts::sync1,
+			consts::sync2,
+			message.addresses.destination,
+			message.addresses.source,
+			payload_length
+		);
 
-        dest[0] = consts::sync1;
-        dest[1] = consts::sync2;
+		const auto crc_h = CastagnoliCRC32::calc(start.take(consts::link_header_fields_size));
 
-        UInt16::write(dest.skip(2), message.addresses.destination);
-		UInt16::write(dest.skip(4), message.addresses.source);
-        UInt16::write(dest.skip(6), payload_length);
+		UInt32::write_to(dest, crc_h);
 
-        // append the header crc
-        const auto crc_h = CastagnoliCRC32::calc(dest.as_rslice().take(consts::link_header_fields_size));
-        UInt32::write(dest.skip(consts::link_header_fields_size), crc_h);
-
-        // copy the payload
-        auto payload_start = dest.skip(consts::link_header_total_size);
-		message.payload.copy_to(payload_start);
+		// copy the payload
+		message.payload.copy_to(dest);
 
         // append the body crc
-        UInt32::write(dest.skip(consts::link_header_total_size + payload_length), CastagnoliCRC32::calc(message.payload));
+        UInt32::write_to(dest, CastagnoliCRC32::calc(message.payload));
 
-        return dest.as_rslice().take(consts::min_link_frame_size + payload_length);
+        return start.take(consts::min_link_frame_size + payload_length);
     }
 }
 
