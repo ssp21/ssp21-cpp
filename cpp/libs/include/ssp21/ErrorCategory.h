88ce75580da8f8bac1f8d6cd99f6204a7d72be82
jadamcrain <jadamcrain@gmail.com>
2016-09-05 02:38:58 -0400

    Move error category template to its own file

---
+++ cpp/libs/include/ssp21/ErrorCategory.h
@@ -0,0 +1,45 @@
+
+#ifndef SSP21_ERRORCATEGORY_H
+#define SSP21_ERRORCATEGORY_H
+
+#include <system_error>
+#include <string>
+
+#if (defined _MSC_VER && (_MSC_VER < 1900))
+#define NOEXCEPT
+#else
+#define NOEXCEPT noexcept
+#endif
+
+namespace ssp21
+{
+
+	template <class EnumSpec>
+    class ErrorCategory final : public std::error_category
+    {
+    public:
+
+        static const std::error_category& get()
+        {
+            static ErrorCategory instance;
+            return instance;
+        }
+
+        virtual const char* name() const NOEXCEPT
+        {
+            return EnumSpec::name;
+        }
+
+        virtual std::string message(int ev) const
+        {
+            return EnumSpec::to_string(static_cast<typename EnumSpec::enum_type_t>(ev));
+        }
+
+    private:
+
+        ErrorCategory() {}
+        ErrorCategory(const ErrorCategory&) = delete;
+    };
+}
+
+#endif
--- cpp/libs/include/ssp21/crypto/CryptoErrorCategory.h
+++ cpp/libs/include/ssp21/crypto/CryptoErrorCategory.h
@@ -2,49 +2,12 @@
 #ifndef SSP21_ERROR_CODES_H
 #define SSP21_ERROR_CODES_H
 
-#include <system_error>
-#include <string>
-
+#include "ssp21/ErrorCategory.h"
 #include "ssp21/crypto/gen/CryptoError.h"
-
-#if (defined _MSC_VER && (_MSC_VER < 1900))
-#define NOEXCEPT
-#else
-#define NOEXCEPT noexcept
-#endif
 
 namespace ssp21
 {
-
-	template <class EnumSpec>
-    class ErrorCategory final : public std::error_category
-    {
-    public:
-
-        static const std::error_category& get()
-        {
-            static ErrorCategory instance;
-            return instance;
-        }
-
-        virtual const char* name() const NOEXCEPT
-        {
-            return EnumSpec::name;
-        }
-
-        virtual std::string message(int ev) const
-        {
-            return EnumSpec::to_string(static_cast<CryptoError>(ev));
-        }
-
-    private:
-
-        ErrorCategory() {}
-        ErrorCategory(const ErrorCategory&) = delete;
-    };
-
 	typedef ErrorCategory<CryptoErrorSpec> CryptoErrorCategory;
-
 
     inline std::error_code make_error_code(CryptoError err)
     {
