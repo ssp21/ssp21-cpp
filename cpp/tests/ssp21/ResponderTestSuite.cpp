7fb6a5a5e8db831a63f74bf1b4a47eba7dc2b46c
Adam Crain <jadamcrain@gmail.com>
2016-11-03 11:22:59 -0400

    moved the responder fixture to its own file

--- cpp/tests/ssp21/ResponderTestSuite.cpp
+++ cpp/tests/ssp21/ResponderTestSuite.cpp
@@ -1,89 +1,10 @@
 
-
-#include "catch.hpp"
-
-#include "ssp21/crypto/Responder.h"
-
-#include "openpal/container/StaticBuffer.h"
-
-#include "testlib/Hex.h"
-#include "testlib/MockExecutor.h"
-
-#include "mocks/MockLogHandler.h"
-#include "mocks/MockLowerLayer.h"
-#include "mocks/MockUpperLayer.h"
-#include "mocks/HexMessageBuilders.h"
-#include "mocks/MakeUnique.h"
-#include "mocks/MockCryptoBackend.h"
+#include "fixtures/ResponderFixture.h"
 
 #define SUITE(name) "ResponderTestSuite - " name
 
 using namespace ssp21;
 using namespace openpal;
-
-class ResponderFixture
-{
-private:
-
-    struct Keys
-    {
-        Keys(BufferType key_type) :
-            local_kp(std::make_unique<KeyPair>()),
-            remote_static_key(std::make_unique<PublicKey>())
-        {
-            init_key(local_kp->private_key, key_type);
-            init_key(local_kp->public_key, key_type);
-            init_key(*remote_static_key, key_type);
-        }
-
-        static void init_key(BufferBase& buffer, BufferType key_type)
-        {
-            buffer.get_write_slice().set_all_to(0xFF);
-            buffer.set_type(key_type);
-        }
-
-        std::unique_ptr<KeyPair> local_kp;
-        std::unique_ptr<PublicKey> remote_static_key;
-    };
-
-
-public:
-
-    ResponderFixture(BufferType key_type = BufferType::x25519_key, const Responder::Config& config = Responder::Config()) :
-        keys(key_type),
-        log("responder"),
-        exe(MockExecutor::Create()),
-        lower(),
-        responder(config, std::move(keys.local_kp), std::move(keys.remote_static_key), log.logger, exe, lower),
-        upper(responder)
-    {
-        MockCryptoBackend::instance.counters.clear();
-    }
-
-    void test_handshake_error(const std::string& request, HandshakeError expected_error)
-    {
-        this->lower.enqueue_message(Addresses(1, 10), request);
-        REQUIRE(this->lower.num_rx_messages() == 1);
-        this->responder.on_rx_ready();
-        REQUIRE(this->lower.num_rx_messages() == 0);
-        REQUIRE(MockCryptoBackend::instance.counters.all_zero());
-        REQUIRE(this->lower.pop_tx_message() == hex::reply_handshake_error(expected_error));
-    }
-
-private:
-
-    Keys keys;
-
-public:
-
-    MockLogHandler log;
-    std::shared_ptr<MockExecutor> exe;
-    MockLowerLayer lower;
-    Responder responder;
-    MockUpperLayer upper;
-};
-
-
 
 TEST_CASE(SUITE("responds to REQUEST_HANDSHAKE_BEGIN with REPLY_HANDSHAKE_BEGIN"))
 {
