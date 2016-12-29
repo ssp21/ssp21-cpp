b73feeb3d4cdc2a1c83c68e34af3d6b7fe987fbe
Adam Crain <jadamcrain@gmail.com>
2016-12-29 14:53:56 -0500

    alias openpal::RSlice as Seq32 for consistency

--- cpp/libs/include/ssp21/crypto/ICryptoBackend.h
+++ cpp/libs/include/ssp21/crypto/ICryptoBackend.h
@@ -18,13 +18,13 @@ namespace ssp21
 
         virtual bool secure_equals(const Seq8& lhs, const Seq8& rhs) = 0;
 
-        virtual void hash_sha256(std::initializer_list<openpal::RSlice> data, SecureBuffer& output) = 0;
+        virtual void hash_sha256(std::initializer_list<Seq32> data, SecureBuffer& output) = 0;
 
-        virtual void hmac_sha256(const Seq8& key, std::initializer_list<openpal::RSlice> data, SecureBuffer& output) = 0;
+        virtual void hmac_sha256(const Seq8& key, std::initializer_list<Seq32> data, SecureBuffer& output) = 0;
 
         virtual void gen_keypair_x25519(KeyPair& pair) = 0;
 
-        virtual void dh_x25519(const PrivateKey& priv_key, const openpal::RSlice& pub_key, DHOutput& output, std::error_code& ec) = 0;
+        virtual void dh_x25519(const PrivateKey& priv_key, const Seq32& pub_key, DHOutput& output, std::error_code& ec) = 0;
     };
 }
 
