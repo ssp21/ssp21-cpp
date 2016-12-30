d6edc509dc3577c82a9a311670a9cd7c4e637ad5
Adam Crain <jadamcrain@gmail.com>
2016-12-30 11:11:09 -0500

    define wseq32_t

--- cpp/libs/include/ssp21/SequenceTypes.h
+++ cpp/libs/include/ssp21/SequenceTypes.h
@@ -2,15 +2,15 @@
 #ifndef SSP21_SEQUENCETYPES_H
 #define SSP21_SEQUENCETYPES_H
 
-#include "openpal/container/RSeq.h"
-
-#include <cstdint>
+#include "openpal/container/WSeq.h"
 
 namespace ssp21
 {
     typedef openpal::RSeq<uint8_t> seq8_t;
     typedef openpal::RSeq<uint16_t> seq16_t;
     typedef openpal::RSeq<uint32_t> seq32_t;
+
+	typedef openpal::WSeq<uint32_t> wseq32_t;
 }
 
 #endif
