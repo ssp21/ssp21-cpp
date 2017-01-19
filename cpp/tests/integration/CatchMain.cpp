7ebfff0f529b4318c88fd853d87c133572a68f2f
jadamcrain <jadamcrain@gmail.com>
2017-01-18 19:39:21 -0500

    added placeholder exe for the integration test suite

---
+++ cpp/tests/integration/CatchMain.cpp
@@ -0,0 +1,39 @@
+
+#define CATCH_CONFIG_RUNNER
+#include "catch.hpp"
+
+#include "sodium/Initialize.h"
+
+int main(int argc, char*  argv[])
+{
+    // global setup...
+    if (argc < 2)
+    {
+        std::cerr << "You did not specify a backend for the integration test" << std::endl;
+        return 1;
+    }
+    else
+    {
+        if (strcmp(argv[1], "--sodium") == 0)
+        {
+            if(!ssp21::sodium::initialize())
+            {
+                std::cerr << "Unable to initialize sodium backend" << std::endl;
+                return 1;
+            }
+        }
+        else
+        {
+            std::cerr << "Unknown backend flag: " << argv[1] << std::endl;
+            return 1;
+        }
+    }
+
+    // massage the arguments to catch so that the backend flag is ignored
+    argv[1] = argv[0];
+    int result = Catch::Session().run(argc - 1, argv + 1);
+
+    // global clean-up...
+
+    return result;
+}
