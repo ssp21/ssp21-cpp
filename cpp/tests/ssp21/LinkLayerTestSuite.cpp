471934a95db7aa81e149ac83b99b8f559743a2e6
Adam Crain <jadamcrain@gmail.com>
2017-03-03 11:33:59 -0500

    test upper layer is opened

--- cpp/tests/ssp21/LinkLayerTestSuite.cpp
+++ cpp/tests/ssp21/LinkLayerTestSuite.cpp
@@ -9,8 +9,12 @@
 using namespace ssp21;
 using namespace openpal;
 
-TEST_CASE(SUITE("construction"))
+TEST_CASE(SUITE("opens upper when opened"))
 {
     LinkLayerFixture fix;
+	
+	REQUIRE_FALSE(fix.upper.get_is_open());
+	fix.link.on_open();
+	REQUIRE(fix.upper.get_is_open());
 }
 
