74320622f0746c01d97a82f3f0186e6f6537d621
Adam Crain <jadamcrain@gmail.com>
2017-03-30 13:21:09 -0400

    add key generation tool

---
+++ cpp/exe/keygen/main.cpp
@@ -0,0 +1,26 @@
+#include "sodium\Initialize.h"
+
+#include "ssp21/crypto/Crypto.h"
+#include "ssp21/util/Base64.h"
+
+#include <iostream>
+
+using namespace ssp21;
+
+int main(int argc, char*  argv[])
+{
+	if (!sodium::initialize()) return -1;
+
+	KeyPair kp;
+	Crypto::gen_keypair_x25519(kp);
+
+	std::cout << "public key:" << std::endl;
+	Base64::encode(kp.public_key.as_seq(), [](char c) { std::cout << c; });
+	std::cout << std::endl;
+
+	std::cout << "private key:" << std::endl;
+	Base64::encode(kp.private_key.as_seq(), [](char c) { std::cout << c; });
+	std::cout << std::endl;
+
+	return 0;
+}
