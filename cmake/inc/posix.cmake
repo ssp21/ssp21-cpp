529992126be27ab6c59eea3b48d7ae688a7a9571
Adam Crain <jadamcrain@gmail.com>
2017-04-04 14:14:28 -0400

    tweaks for older cmake verison

--- cmake/inc/posix.cmake
+++ cmake/inc/posix.cmake
@@ -30,8 +30,8 @@ set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wno-parentheses --std=c++14")
 
 # Compiler-specific detection
 if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-	if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "4.9.3")
-        	message(FATAL_ERROR "GCC version 4.9.3 or greater is required")
+	if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "4.9.0")
+        	message(FATAL_ERROR "GCC version 4.9.0 or greater is required")
 	endif()	
 elseif ("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang")
 	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
