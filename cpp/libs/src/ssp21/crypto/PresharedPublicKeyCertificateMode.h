f00154f371aa3a4c0e37de8f497a1489cb4d6bd9
Adam Crain <jadamcrain@gmail.com>
2017-06-23 13:27:26 -0400

    move modest impl to header file

--- cpp/libs/src/ssp21/crypto/PresharedPublicKeyCertificateMode.h
+++ cpp/libs/src/ssp21/crypto/PresharedPublicKeyCertificateMode.h
@@ -34,7 +34,14 @@ namespace ssp21
             return seq32_t::empty();
         }
 
-        virtual HandshakeError validate(const ssp21::seq32_t& certificates, seq32_t& public_key_output) override;
+		virtual HandshakeError validate(const ssp21::seq32_t& certificates, seq32_t& public_key_output) override
+		{
+			if (certificates.is_not_empty()) return HandshakeError::bad_certificate_format;
+
+			public_key_output = this->remote_static_public_key->as_seq();
+
+			return HandshakeError::none;
+		}
 
 
     private:
