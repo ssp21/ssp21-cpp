b236ea83e437ac7fb5801ed04525276a17bb3908
Adam Crain <jadamcrain@gmail.com>
2017-06-27 11:01:08 -0400

    restore preshared key mode until we can run all tests w/ certificate mode

--- cpp/tests/integration/fixtures/IntegrationFixture.cpp
+++ cpp/tests/integration/fixtures/IntegrationFixture.cpp
@@ -23,8 +23,7 @@ namespace ssp21
         rlog("responder"),
         initiator_lower(exe),
         responder_lower(exe),
-        //stacks(preshared_key_stacks(rlog.logger, ilog.logger, exe))
-        stacks(certificate_stacks(rlog.logger, ilog.logger, exe))
+        stacks(preshared_key_stacks(rlog.logger, ilog.logger, exe))
     {
         this->wire();
     }
