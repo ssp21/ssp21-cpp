ae44373fd5c2819a0ed9d946a5aeee39790dfcfd
jadamcrain <jadamcrain@gmail.com>
2017-06-29 10:37:18 -0700

    finish the single level certificate script

--- script/generate_single_level_certificates.sh
+++ script/generate_single_level_certificates.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-echo "Generating a DSA key pair for the authority"
+echo "// Generating a DSA key pair for the authority"
 ./icftool -e authority.priv.icf authority.pub.icf
+echo ""
 
-echo "Creating a self-signed certificate for the authority"
-./icftool -c authority.cert.icf authority.pub.icf authority.priv.icf -d365 -l0
+echo "// Creating a self-signed certificate for the authority"
+./icftool -c authority.cert.icf authority.pub.icf authority.priv.icf -d365 -l1
+echo ""
 
-echo "Generating DH static key pairs for the initiator and the responder"
+echo "// Generating DH static key pairs for the responder"
 ./icftool -x responder.priv.icf responder.pub.icf
-./icftool -x initiator.priv.icf responder.pub.icf
+echo ""
 
+echo "// Creating a certificate for the responder signed by authority private key"
+./icftool -c responder.cert.icf responder.pub.icf authority.priv.icf -d100 -l0
+echo ""
+
+echo "// Generating DH static key pairs for the initiator"
+./icftool -x initiator.priv.icf initiator.pub.icf
+echo ""
+
+echo "// Creating a certificate for the initiator signed by authority private key"
+./icftool -c initiator.cert.icf initiator.pub.icf authority.priv.icf -d100 -l0
+echo ""
 
