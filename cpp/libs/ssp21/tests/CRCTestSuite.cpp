362a09eada8534cd4dbf867168ce2fab5cb07f4c
jadamcrain <jadamcrain@gmail.com>
2019-05-07 11:28:21 -0700

    add additional test vector from Qubitekk document

--- cpp/libs/ssp21/tests/CRCTestSuite.cpp
+++ cpp/libs/ssp21/tests/CRCTestSuite.cpp
@@ -12,12 +12,19 @@ using namespace ser4cpp;
 
 /*
 
-These test vector both agree with:
+These test vectors agree with:
 
-https://www.ghsi.de/CRC/
 http://www.sunshine2k.de/coding/javascript/crc/crc_js.html
 
-**/
+The proper settings for the online generator are:
+CRC Width = CRC-32
+Input Reflected = No
+Result Reflected = No
+Polynomial = 0xf4acfb13
+Initial Value = 0x0000000
+Final XOR Value = 0x0000000
+
+*/
 
 TEST_CASE(SUITE("Castagnoli test vector #1"))
 {
@@ -29,5 +36,12 @@ TEST_CASE(SUITE("Castagnoli test vector #2"))
 {
     const auto hex = HexConversions::from_hex("DE AD BE EF");
     REQUIRE(CastagnoliCRC32::calc(hex->as_rslice()) == 0xBB4E2049);
+}
+
+TEST_CASE(SUITE("Castagnoli test vector #3"))
+{
+	const std::string data("abcdefghijklmnopqrstuvwxyz012345");
+	const auto slice = rseq_t(reinterpret_cast<const uint8_t*>(data.c_str()), static_cast<uint32_t>(data.length()));
+	REQUIRE(CastagnoliCRC32::calc(slice) == 0x242D5EBD);
 }
 
